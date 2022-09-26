b5b6d417a5110f6c09b87d36e4d3a0794e9c2742
Ian <new@news-MacBook-Pro.local>
2022-09-26 08:07:25 -0600

    file structure change. revert to static html files

--- src/pages/Contact-ModelInfo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ContactModelInfo() {
-  return (
-    <div>
-        
-    </div>
-  )
-}
--- src/pages/ModelComments.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ModelComments() {
-  return (
-    <div>
-        
-    </div>
-  )
-}
